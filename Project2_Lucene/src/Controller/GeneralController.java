ad7be89d47830c408451d1730334dc2a3b3d746f
Javier Rivera <71288541+javirima@users.noreply.github.com>
2021-10-30 02:02:05 -0600

    Update controller

--- Project2_Lucene/src/Controller/GeneralController.java
+++ Project2_Lucene/src/Controller/GeneralController.java
@@ -7,6 +7,20 @@ package Controller;
 
 import Model.PathChooser;
 import java.io.IOException;
+import Model.Indexer;
+import Model.QuerySearcher;
+import Model.StopWordsFile;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import View.QuerySearcherView;
+import View.IndexerView;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+
 
 
 /**
@@ -35,5 +49,38 @@ public class GeneralController {
        return path;
     }
     
-
+    public void searchQuery(String index,String busqueda){
+        QuerySearcher searcher = new QuerySearcher();
+        ArrayList<Document> documents = null;
+        try {
+            documents = searcher.Search(index,busqueda);
+        } catch (IOException ex) {
+            Logger.getLogger(GeneralController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(GeneralController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (Document doc: documents){
+            System.out.println(doc);
+        }
+        
+        
+    }
+    
+    public void createIndex(IndexerView vistaIndice,String collection, String index, String stopwords){
+        Indexer ind = new Indexer();
+        StopWordsFile sf = new StopWordsFile(stopwords);
+        List<String> stopWords = sf.readTxt(); 
+        
+        try {
+            ind.Index(index, collection, stopWords);
+        } catch (IOException ex) {
+            Logger.getLogger(GeneralController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        vistaIndice.setVisible(false);
+        QuerySearcherView searchView = new QuerySearcherView();
+        searchView.setVisible(true);
+        
+    }
+    
+    
 }
